d51ae47e86b683862433029d79a02934e6229b47
Caique de Castro <caiquecastro@outlook.com>
2019-07-05 18:01:06 -0300

    Fix BC for Tailwind 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#444444",
+        blue: "#3490dc",
+        "blue-darker": "#1c3d5a"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
